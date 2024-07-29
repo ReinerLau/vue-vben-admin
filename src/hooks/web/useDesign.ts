ee3b724cac116d223f4673d4f6b895dd55bf5700
ReinerLau <lk850593913@gmail.com>
2024-07-29 10:26:52 +0800

    docs: 定义样式作用域的 hook

--- src/hooks/web/useDesign.ts
+++ src/hooks/web/useDesign.ts
@@ -1,9 +1,12 @@
-/**
- * // [x]
- * @description: 为组件提供样式相关的属性的 hook
- */
 import { useAppProviderContext } from '@/components/Application';
 
+/**
+ * 定义样式作用域的 hook
+ * @param scope - 自定义样式作用域标识
+ * @returns {Object} result - 一个对象
+ * @returns {string} result.prefixCls - 样式作用域类名 = 样式作用域前缀 + 自定义作用域标识
+ * @returns {string} result.prefixVar - 样式作用域前缀，默认为 vben
+ */
 export function useDesign(scope: string) {
   const values = useAppProviderContext();
 
