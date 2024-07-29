4a43b607a918534ab49f11ca59623d9b603fe34d
ReinerLau <lk850593913@gmail.com>
2024-07-29 10:55:40 +0800

    docs: useAppContext

--- src/components/Application/src/useAppContext.ts
+++ src/components/Application/src/useAppContext.ts
@@ -1,7 +1,3 @@
-/**
- * // [x]
- * @description 赋予 AppProvider 组件提供上下文能力的 hook
- */
 import { createContext, useContext } from '@/hooks/core/useContext';
 import { InjectionKey, Ref } from 'vue';
 
@@ -10,12 +6,24 @@ export interface AppProviderContextProps {
   isMobile: Ref<boolean>;
 }
 
+/**
+ * 注入来自 AppProvider 组件的上下文时使用的 key
+ */
 const key: InjectionKey<AppProviderContextProps> = Symbol();
 
+/**
+ * AppProvider 组件提供上下文
+ * @param context - 要提供的上下文
+ * @returns 响应式处理后的上下文
+ */
 export function createAppProviderContext(context: AppProviderContextProps) {
   return createContext<AppProviderContextProps>(context, key);
 }
 
+/**
+ * 获取来自 AppProvider 组件的上下文
+ * @returns AppProvider 组件提供的上下文
+ */
 export function useAppProviderContext() {
   return useContext<AppProviderContextProps>(key);
 }
