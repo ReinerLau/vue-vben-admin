9abfafbc0262ecd8355da563d287e69363c56928
ReinerLau <lk850593913@gmail.com>
2024-07-31 13:44:59 +0800

    docs: setupStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
-import type { App } from 'vue';
-import { createPinia } from 'pinia';
 import { registerPiniaPersistPlugin } from '@/store/plugin/persist';
+import { createPinia } from 'pinia';
+import type { App } from 'vue';
 
 const store = createPinia();
 registerPiniaPersistPlugin(store);
 
+/**
+ * 注册 Pinia 插件
+ * @param app vue 应用实例
+ */
 export function setupStore(app: App<Element>) {
   app.use(store);
 }
