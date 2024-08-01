14165899567b5684abe95fd795fe35f455c6f8f2
ReinerLau <lk850593913@gmail.com>
2024-08-01 16:09:50 +0800

    docs: useAppStore setProjectConfig

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -95,7 +95,10 @@ export const useAppStore = defineStore({
     setBeforeMiniInfo(state: BeforeMiniState): void {
       this.beforeMiniInfo = state;
     },
-
+    /**
+     * @description 缓存新的项目配置
+     * @param config 新的项配置
+     */
     setProjectConfig(config: DeepPartial<ProjectConfig>): void {
       this.projectConfig = deepMerge(this.projectConfig || {}, config) as ProjectConfig;
       Persistent.setLocal(PROJ_CFG_KEY, this.projectConfig);
