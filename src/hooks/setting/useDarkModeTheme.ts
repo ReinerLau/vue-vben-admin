397080de6d497b979c55f8f7c0e01f6b23e0d430
ReinerLau <lk850593913@gmail.com>
2024-08-02 14:16:39 +0800

    docs: useDarkModeTheme

--- src/hooks/setting/useDarkModeTheme.ts
+++ src/hooks/setting/useDarkModeTheme.ts
@@ -3,6 +3,9 @@ import { useRootSetting } from '@/hooks/setting/useRootSetting';
 import { theme } from 'ant-design-vue';
 import { computed } from 'vue';
 
+/**
+ * 深色主题相关的 hook
+ */
 export function useDarkModeTheme() {
   const { getDarkMode } = useRootSetting();
   const { darkAlgorithm } = theme;
@@ -16,7 +19,10 @@ export function useDarkModeTheme() {
      * 是否为深色模式
      */
     isDark,
-    // TODO
+    /**
+     * antd 自带的深色主题算法
+     * @tutorial https://antdv.com/docs/vue/customize-theme-cn#%E5%AE%9A%E5%88%B6%E4%B8%BB%E9%A2%98
+     */
     darkTheme,
   };
 }
