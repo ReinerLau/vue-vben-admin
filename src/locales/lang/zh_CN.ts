673d973a19569cca679c1a0a896f0cddfb14e4ed
ReinerLau <lk850593913@gmail.com>
2024-08-02 13:55:29 +0800

    docs: zh_CN

--- src/locales/lang/zh_CN.ts
+++ src/locales/lang/zh_CN.ts
@@ -1,9 +1,15 @@
-import { genMessage } from '../helper';
-import antdLocale from 'ant-design-vue/es/locale/zh_CN';
 import { deepMerge } from '@/utils';
+import antdLocale from 'ant-design-vue/es/locale/zh_CN';
+import { genMessage } from '../helper';
 
+/**
+ * 自定义语言包
+ */
 const modules = import.meta.glob('./zh-CN/**/*.{json,ts,js}', { eager: true });
 
+/**
+ * 合并来自 antd 自带语言包和自定义语言包
+ */
 export default {
   message: {
     ...genMessage(modules as Recordable<Recordable>, 'zh-CN'),
