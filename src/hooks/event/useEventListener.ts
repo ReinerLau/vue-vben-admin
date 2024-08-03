8d71407e776b67bc7770c7af4e99d7898054d47f
ReinerLau <lk850593913@gmail.com>
2024-08-03 08:34:01 +0800

    docs: useEventListener

--- src/hooks/event/useEventListener.ts
+++ src/hooks/event/useEventListener.ts
@@ -4,16 +4,41 @@ import { ref, unref, watch } from 'vue';
 
 export type RemoveEventFn = () => void;
 export interface UseEventParams {
+  /**
+   * 监听元素
+   */
   el?: Element | Ref<Element | undefined> | Window | any;
+  /**
+   * 事件名称
+   */
   name: string;
+  /**
+   * 事件监听函数
+   */
   listener: EventListener;
+  /**
+   * addEventListener 选项
+   */
   options?: boolean | AddEventListenerOptions;
+  /**
+   * 监听元素变化后是否自动清除对旧元素的事件监听
+   */
   autoRemove?: boolean;
+  /**
+   * 是否使用防抖
+   */
   isDebounce?: boolean;
+  /**
+   * 防抖时间或节流频率
+   * @description 没有设置则不使用防抖或节流
+   * @description 默认 80ms
+   */
   wait?: number;
 }
 
-// TODO
+/**
+ * 增加事件监听相关 hook
+ */
 export function useEventListener({
   el = window,
   name,
@@ -28,15 +53,31 @@ export function useEventListener({
   const isAddRef = ref(false);
 
   if (el) {
+    /**
+     * 监听元素
+     */
     const element = ref(el as Element) as Ref<Element>;
-
+    /**
+     * 监听函数
+     * @description 默认使用防抖，否则使用节流
+     */
     const handler = isDebounce ? useDebounceFn(listener, wait) : useThrottleFn(listener, wait);
+    /**
+     * 实际的事件处理函数
+     * @description 如果不需要防抖或者节流，则直接使用监听函数
+     */
     const realHandler = wait ? handler : listener;
     const removeEventListener = (e: Element) => {
       isAddRef.value = true;
       e.removeEventListener(name, realHandler, options);
     };
-    const addEventListener = (e: Element) => e.addEventListener(name, realHandler, options);
+    /**
+     * 添加事件监听
+     * @param e 监听元素
+     */
+    const addEventListener = (e: Element) => {
+      e.addEventListener(name, realHandler, options);
+    };
 
     const removeWatch = watch(
       element,
