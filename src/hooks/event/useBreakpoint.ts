f7d1af0c05c517c8d3016aff79eca848a12ce793
ReinerLau <lk850593913@gmail.com>
2024-08-03 08:57:57 +0800

    docs: useBreakpoint

--- src/hooks/event/useBreakpoint.ts
+++ src/hooks/event/useBreakpoint.ts
@@ -6,16 +6,43 @@ import { computed, ComputedRef, ref, unref } from 'vue';
 // import { Grid } from 'ant-design-vue';
 // const { useBreakpoint } = Grid;
 
+/**
+ * 当前尺寸
+ */
 let globalScreenRef: ComputedRef<sizeEnum | undefined>;
+/**
+ * 当前尺寸对应的最大屏幕宽度
+ */
 let globalWidthRef: ComputedRef<number>;
+/**
+ * 当前实际屏幕宽度
+ */
 let globalRealWidthRef: ComputedRef<number>;
 
 export interface CreateCallbackParams {
+  /**
+   * 当前尺寸
+   */
   screen: ComputedRef<sizeEnum | undefined>;
+  /**
+   * 当前尺寸对应的最大屏幕宽度
+   */
   width: ComputedRef<number>;
+  /**
+   * 当前实际屏幕宽度
+   */
   realWidth: ComputedRef<number>;
+  /**
+   * 尺寸对应的最大屏幕宽度枚举
+   */
   screenEnum: typeof screenEnum;
+  /**
+   * 尺寸与屏幕宽度的映射
+   */
   screenMap: Map<sizeEnum, number>;
+  /**
+   * 尺寸枚举
+   */
   sizeEnum: typeof sizeEnum;
 }
 
@@ -28,20 +55,22 @@ export function useBreakpoint() {
   };
 }
 
-// TODO
-// Just call it once
+/**
+ * 监听屏幕窗口大小调整
+ * @param fn 屏幕窗口调整大小后触发的回调
+ */
 export function createBreakpointListen(fn?: (opt: CreateCallbackParams) => void) {
   /**
    * 尺寸
    */
   const screenRef = ref<sizeEnum>(sizeEnum.XL);
   /**
-   * 屏幕宽度
+   * 实际屏幕宽度
    */
   const realWidthRef = ref(window.innerWidth);
 
   /**
-   * 获取尺寸和屏幕宽度
+   * 获取尺寸和实际屏幕宽度
    */
   function getWindowWidth() {
     const width = document.body.clientWidth;
@@ -82,6 +111,9 @@ export function createBreakpointListen(fn?: (opt: CreateCallbackParams) => void)
   globalWidthRef = computed((): number => screenMap.get(unref(screenRef)!)!);
   globalRealWidthRef = computed((): number => unref(realWidthRef));
 
+  /**
+   * 屏幕窗口调整大小后触发回调
+   */
   function resizeFn() {
     fn?.({
       screen: globalScreenRef,
